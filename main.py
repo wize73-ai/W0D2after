b26b0e1397d226fd6eb0a25b1906233ffe071d24
james wilson <jawarm24@gmail.com>
2024-09-19 10:53:07 -0400

    added greet-person

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-```python
+# ```python
 # main.py
 def hello_world():
     return "Hello from James!"
 
+def greet_person(name):
+    return(f"Hello, {name}")
+
 if __name__ == "__main__":
     print(hello_world())
-```
+# ```
--- test_main.py
+++ test_main.py
@@ -1,12 +1,15 @@
-```python
+# ```python
 # test_main.py
 import unittest
-from main import hello_world
+from main import hello_world, greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello from James!")
 
+    def test_greet_person(self):
+        self.assertEqual(greet_person("James"), "Hello, James")
+
 if __name__ == '__main__':
     unittest.main()
-```
+# ```
